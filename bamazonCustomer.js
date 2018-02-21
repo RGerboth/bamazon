7e714e176b10268fb42fdd83d14463172147d348
Robert Gerboth <email@example.com>
2018-02-21 08:47:02 -0700

    changed flow to enable controlled exit of order loop, minor UI changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -51,6 +51,7 @@ promptUser = function() {
 						if (error) throw error;
 						var orderTotal = prodPrice * numOrdered
 						console.log("  Thank you for your order of " + numOrdered + " " + prodNameOrdered + " at $" + (prodPrice/100).toFixed(2) + " a piece. Your total is " + "$" + (orderTotal/100).toFixed(2) + ".")
+						console.log("")
 						placeOrder() 
 					})	
 				} else {
@@ -61,47 +62,45 @@ promptUser = function() {
 				}
 			} else {
 				console.log(" ")
-				console.log("Sorry, " + response.productOrdered + " " + response.quantityOrdered + " is not a valid order. You must specify a valid product and quantity.")
+				console.log("Sorry, " + response.productOrdered + " " + response.quantityOrdered + " is not a valid order. Please specify a valid product and quantity.")
 				placeOrder() 
 			}
 		})
 	}
 
 placeOrder = function() {
-	console.log("")
-	console.log("Here are the items in stock: ")
-	console.log("")
-	con.query('SELECT * FROM `products`', function (error, results, fields) {
-		prodArray = results
-		for (i=0; i<prodArray.length; i++) {
-			console.log(prodArray[i].id + ":  " + prodArray[i].product_name)
-			console.log("  Price: $" + (prodArray[i].price/100).toFixed(2) + ",  " + prodArray[i].stock_quantity + " available.")
+	inquirer.prompt([ //place this whole thing inside placeOrder
+	    {
+	      type: "confirm",
+	      message: "Would you like to place an order?",
+	      name: "confirm",
+	      default: true
+	  	}
+	]).then(function(response) {
+		if (response.confirm) {
+			console.log("")
+			console.log("Great, let's get started. ")
+			console.log("")
+			console.log("Here are the items in stock: ")
+			console.log("")
+			con.query('SELECT * FROM `products`', function (error, results, fields) {
+				prodArray = results
+				for (i=0; i<prodArray.length; i++) {
+					console.log(prodArray[i].id + ":  " + prodArray[i].product_name)
+					console.log("      Price: $" + (prodArray[i].price/100).toFixed(2) + ",  " + prodArray[i].stock_quantity + " available.")
+				}
+				console.log("")
+				promptUser()
+			});
+
+		} else {
+			console.log("")
+			console.log("   Hey, no problem! Thank you for trying Bamazon.")
+			con.end(function(err) {
+				console.log("   Please come back again some time.")
+			})
 		}
-		console.log("")
-		promptUser()
-	});
+	})
 }
 
-inquirer.prompt([ //place this whole thing inside placeOrder
-    {
-      type: "confirm",
-      message: "Would you like to place an order?",
-      name: "confirm",
-      default: true
-  	}
-]).then(function(response) {
-	if (response.confirm) {
-		console.log("")
-		console.log("Great, let's get started. ")
-		placeOrder() //insert current placeOrder functions here
-
-	} else {
-		console.log("")
-		console.log("Hey, no problem...")
-		con.end(function(err) {
-			console.log("   please come back again some time.")
-		})
-	}
-})
-
-//placeOrder()
+placeOrder()
