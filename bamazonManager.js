a678418ad99d7a81d419894f3dd8e4fbab9aeed1
Robert Gerboth <email@example.com>
2018-02-22 07:32:40 -0700

    bamazonManager.js complete and tested

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,88 +1,125 @@
 //bamazon
 //manager view
-// USE bamazon_db;
+
 var inquirer = require("inquirer")
 var mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "root"
-// });
+var prodArray = []
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+var con = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  database: "bamazon_db",
+  user: "root",
+  password: "root"
+});
 
-console.log("---------- Welcome to Bamazon ----------")
-console.log("-------- You are in Manager mode -------")
-console.log("")
-inquirer.prompt([
-    {
-      type: "list",
-      message: "What would you like to do?",
-      choices: ["See All Products", "View Low Inventory", "Add to Inventory", "Add New Product"],
-      name: "action"
-    }
-]).then(function(response) {
-	if (response.action == "View Low Inventory") {
-		console.log("")
-		console.log("View Low Inventory. ")
-		console.log("")
-	} else if (response.action == "Add to Inventory") {
-		console.log("")
-		console.log("FUNCTION: Add to Inventory. ")
-		console.log("")
-		inquirer.prompt([
-		    {
-		      type: "input",
-		      message: "What product would you like to update?",
-		      name: "productID"
-		    },
-			{
-		      type: "input",
-		      message: "What is the quantity added?",
-		      name: "quantityAdded"
-		    }
-		]).then(function(response) {
-			var newQuantity = 0
-			//SELECT FROM proucts 
-			//WHERE id = response.productID
-			//newQuantity = response.quantityAdded + stock_quantity
-			// UPDATE products
-			// SET stock_quantity=newQuantity
-			// WHERE id = response.productID;
-			console.log("Product Updated")
-		})
-	} else if (response.action == "Add New Product") {
-		console.log("")
-		console.log("FUNCTION: Add New Product. ")
-		console.log("")
-		inquirer.prompt([
-		    {
-		      type: "input",
-		      message: "What would you like to add?",
-		      name: "newProduct"
-		    },
-			{
-		      type: "input",
-		      message: "What is the product price?",
-		      name: "newProductPrice"
-		    },
-		    {
-		      type: "input",
-		      message: "What is the starting inventory?",
-		      name: "newProductInventory"
-		    }
-		]).then(function(response) {
-			// INSERT INTO products (product_name, price, stock_quantity)
-			// VALUES (response.newProduct, response.ProductPrice, response.ProductInventory);
-			console.log("Product Added")
-		})
-	} else {
-		console.log("")
-		console.log("Here are ALL PRODUCUTS. ")
-		console.log("")
-	}
-})
+con.connect(function(err) {
+  if (err) throw err;
+});
+
+console.log("\n|-------------------------------------------------|")
+console.log("|--------------- Welcome to Bamazon --------------|")
+console.log("|------------------ MANAGER MODE -----------------|")
+console.log("|-------------------------------------------------|\n")
+
+start = function() {
+	inquirer.prompt([
+	    {
+	      type: "list",
+	      message: "What would you like to do?",
+	      choices: ["See All Products", "View Low Inventory", "Update Inventory", "Add New Product", "Exit"],
+	      name: "action"
+	    }
+	]).then(function(response) {
+		if (response.action == "See All Products") {
+			con.query('SELECT * FROM `products`', function (error, results, fields) {
+				prodArray = results
+				for (i=0; i<prodArray.length; i++) {
+					console.log(prodArray[i].id + ":  " + prodArray[i].product_name)
+					console.log("      Price: $" + (prodArray[i].price/100).toFixed(2) + ",  " + prodArray[i].stock_quantity + " available.\n")
+				}
+				console.log("")
+				start()
+			});
+		} else if (response.action == "View Low Inventory") {
+			var lowInventory = 5
+			con.query("SELECT * FROM products WHERE stock_quantity < ?", [lowInventory], function(error,results,fields) {
+				prodArray = results
+				if (prodArray.length < 1) {
+					console.log("  \nALL INVENTORY UP TO MINIMUM LEVEL\n")
+					start()
+				} else {
+					for (i=0; i<prodArray.length; i++) {
+						console.log(prodArray[i].id + ":  " + prodArray[i].product_name)
+						console.log("      Price: $" + (prodArray[i].price/100).toFixed(2) + ",  " + prodArray[i].stock_quantity + " available.\n")
+					}
+					start()
+				}
+			});
+		} else if (response.action == "Update Inventory") {
+			con.query('SELECT * FROM `products`', function (error, results, fields) {
+				prodArray = results
+				for (i=0; i<prodArray.length; i++) {
+					console.log(prodArray[i].id + ":  " + prodArray[i].product_name)
+					console.log("      Price: $" + (prodArray[i].price/100).toFixed(2) + ",  " + prodArray[i].stock_quantity + " available.\n")
+				}
+				inquirer.prompt([
+				    {
+				      type: "input",
+				      message: "What product number would you like to update?",
+				      name: "productID"
+				    },
+					{
+				      type: "input",
+				      message: "What is the quantity added or (-)removed?",
+				      name: "quantityAdded"
+				    }
+				]).then(function(response) {
+					var newQuantity = parseInt(response.quantityAdded) + prodArray[response.productID-1].stock_quantity
+					con.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [newQuantity, response.productID], function(error,results,fields) {
+						console.log("\nProduct: " + response.productID + " (" + prodArray[response.productID-1].product_name + ") new quantity: " + newQuantity + "\n")
+						console.log("Product Updated \n")
+						start()
+						})
+					})
+			})
+
+		} else if (response.action == "Add New Product") {
+			inquirer.prompt([
+			    {
+			      type: "input",
+			      message: "What is the name of the new product?",
+			      name: "newProduct"
+			    },
+				{
+			      type: "input",
+			      message: "What is the product price?",
+			      name: "newProductPrice"
+			    },
+			    {
+			      type: "input",
+			      message: "What is the starting inventory?",
+			      name: "newProductInventory"
+			    }
+			]).then(function(response) {
+				con.query("INSERT INTO products SET ?",
+			    {
+			      product_name: response.newProduct,
+			      price: parseInt(response.newProductPrice),
+			      stock_quantity: parseInt(response.newProductInventory)
+			    }, function(error,results,fields) {
+					console.log("\nProduct: " + response.newProduct + " price $" + (response.newProductPrice/100).toFixed(2) + " quantity " + response.newProductInventory + " added. \n")
+					console.log("\nProduct Added\n")
+					start()
+				})
+			})
+		} else {
+			con.end(function(err) {
+				console.log("\nYou have successfully logged out of manager mode. \n")
+			})
+		}
+	})
+}
+
+start()
